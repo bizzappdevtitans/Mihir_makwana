d3e69ea8421f01d6cf3e1e0a44cacfe46baaebae
name <name.surname@bizzappdev.com>
2023-10-26 11:00:18 +0530

    [WIP][T00472]corporate_it_service : change in models

--- corporate_it_service/models/applicant_stage.py
+++ corporate_it_service/models/applicant_stage.py
@@ -9,20 +9,20 @@ class ApplicantStages(models.Model):
     sequence = fields.Integer(string="Secuence", default="10")
     requirement = fields.Text(string="Recruitment")
     template_id = fields.Many2one(comodel_name="mail.template", string="Email Template")
-    legend_blocked = fields.Char(
-        "Red Kanban Label",
+    blocked = fields.Char(
+        "Red  Label",
         default=lambda self: _("Blocked"),
         translate=True,
         required=True,
     )
-    legend_done = fields.Char(
-        "Green Kanban Label",
+    done = fields.Char(
+        "Green  Label",
         default=lambda self: _("Ready for Next Stage"),
         translate=True,
         required=True,
     )
-    legend_normal = fields.Char(
-        "Grey Kanban Label",
+    normal = fields.Char(
+        "Grey  Label",
         default=lambda self: _("In Progress"),
         translate=True,
         required=True,
