7885755d4f198a12a4fab08458e1720b26089e41
name <name.surname@bizzappdev.com>
2023-10-30 18:33:39 +0530

    [WIP][T00380]business-management : added business management module

--- business_management/models/account_move.py
+++ business_management/models/account_move.py
@@ -5,6 +5,6 @@ class AccountMove(models.Model):
     _inherit = ["account.move"]
 
     # fields for #T000380
-    _customer_number = fields.Char(string="Customer Number")
-    _delivery_description = fields.Text(string="Delivery Description")
+    customer_number = fields.Char()
+    delivery_description = fields.Text()
     _invoice_description = fields.Text(string="Invoice Description")
