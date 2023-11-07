91196037246c5d50d2371bb9a51f32963076458c
name <name.surname@bizzappdev.com>
2023-11-07 19:12:31 +0530

    [WIP][T00394]set_delivery_adress : changes in manifest files

--- set_delivery_adress/__manifest__.py
+++ set_delivery_adress/__manifest__.py
@@ -5,13 +5,13 @@
     "version": "15.0.0.0.1",
     "category": "Address management",
     "summary": "Address management",
-    "author": "Bizzappdev",
-    "website": "https://github.com/OCA/product-variant",
+    "author": "BizzAppDev",
+    "website": "https://www.bizzappdev.com",
     "depends": ["base", "sale"],
     "data": [
         "views/res_partner_views.xml",
     ],
     "installable": True,
     "application": True,
-    "license": "LGPL-3",
+    "license": "Other proprietary",
 }
