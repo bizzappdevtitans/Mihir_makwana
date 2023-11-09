56bb07399e3570ea88d75cd1a7cb262844ffa1ed
name <name.surname@bizzappdev.com>
2023-11-09 11:56:02 +0530

    [WIP][T00477]add_attachment : add Add_attachment module

---
+++ add_attachment/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Add  Attachments",
+    "version": "15.0.0.0.1",
+    "category": "sales/sales",
+    "author": "BizzAppDev",
+    "website": "https://www.bizzappdev.com",
+    "depends": ["base", "sale_management"],
+    "installable": True,
+    "license": "Other proprietary",
+}
