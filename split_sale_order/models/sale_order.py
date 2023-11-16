d68dde83b3dd1e135c0dfd3f4bf4878b66999d42
name <name.surname@bizzappdev.com>
2023-11-16 19:05:24 +0530

    [WIP][T00483]split_sale_order : Added a wizard's split button method

--- split_sale_order/models/sale_order.py
+++ split_sale_order/models/sale_order.py
@@ -1,6 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    sale_order_reference = fields.Char()
--- split_sale_order/wizard/split_sale_order.py
+++ split_sale_order/wizard/split_sale_order.py
@@ -1,13 +1,38 @@
-from odoo import fields, models
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SplitSaleOrderWizard(models.Model):
-    _name = "spli.sale.order.wizard"
+    _name = "split.sale.order.wizard"
     _description = "Split Sale Order"
 
-    is_split_sale_order_based_on_category = fields.Boolean(
-        string="Split SO Based On Category ?"
+    sale_order_id = fields.Many2one(
+        comodel_name="sale.order", string="Sale Order Number"
     )
 
-    def action_confirm_so_order(self):
-        pass
+    is_split_sale_order_based_on_category = fields.Boolean(
+        string="Based On Category ?", default=True
+    )
+
+    def action_confirm_split_sale_order(self):
+        if not self.sale_order_id.sale_order_reference:
+            if self.is_split_sale_order_based_on_category:
+                product_category_list = []
+
+                for products in self.sale_order_id.order_line:
+                    if products.product_id.categ_id.name not in product_category_list:
+                        product_category_list.append(products.product_id.categ_id.name)
+
+                    self.env["sale.order"].create(
+                        {
+                            "partner_id": self.sale_order_id.partner_id.id,
+                            "sale_order_reference": self.sale_order_id,
+                            "order_line": product_category_list,
+                        }
+                    )
+
+            else:
+                raise ValidationError(_("INVALID !!!!!!!!!!!!!"))
+        else:
+            raise ValidationError(_(" ALREADY SPLITED"))
